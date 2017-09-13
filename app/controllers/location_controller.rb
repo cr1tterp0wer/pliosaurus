57812649a3bc098cd41aad03b45bce3b8e43adeb
cr1tterp0wer <capodacac@gmail.com>
2017-09-13 03:26:37 -0700

    bugfix

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -4,7 +4,9 @@ class LocationController < ApplicationController
   end
   def create
     @location = Location.new(location_params)
-                             
+    #ONLY ONE LOCATION PER USER
+    Location.where(user_id: location_params[:user_id]).each {|loc| loc.destroy()}
+
     @location.save
     flash[:errors] = @location.errors.full_messages
     redirect_to user_url(current_user)
